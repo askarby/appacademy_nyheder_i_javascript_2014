f295a7d223937bf80ad96b9d00ed33d00bacd2e9
Anders Skarby <anders@skarby.info>
2014-10-28 20:06:42 +0100

    Added angularjs, controller, dummy data

---
+++ frontend/app.js
@@ -0,0 +1,28 @@
+/*global angular: false*/
+/*jslint es5: true */
+(function (angular) {
+    'use strict';
+
+    var app = angular.module('postit', []),
+        template = { id: undefined, author: 'Anonymous', contents: '' };
+    
+    app.controller('ListCtrl', function ($scope) {
+        $scope.notes = [];
+        
+        this.add = function addNote(text) {
+            var note = angular.copy(template);
+            note.created = Date.now();
+            if (text) {
+                note.contents = text;
+            }
+            $scope.notes.push(note);
+        };
+        
+        this.add('Sample note containing important information...');
+        this.add('Some didn\'t believe that notes could be awesome');
+        this.add('More than awesome...');
+        this.add('Buy milk, and bread');
+        this.add('Technologies to use:\n- NodeJS\n- JavaScript\n- AngularJS');
+        this.add('I couldn\'t come up with anything interesting to type');
+    });
+}(angular));
